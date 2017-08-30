4e821f785f589163de031bdb2394d86587286a8d
Sirko Schindler <sirko.schindler@uni-jena.de>
2017-08-30 14:13:19 +0200

    added methods for file download
    * pending some CORE changes to get them to work

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -3,6 +3,10 @@ using System.Web.Mvc;
 using Vaiona.Web.Mvc.Models;
 using Vaiona.Web.Extensions;
 using System.Collections.Generic;
+using BExIS.IO.Transform.Output;
+using System;
+using System.Data;
+using System.IO;
 
 namespace BExIS.Modules.Lui.UI.Controllers
 {
@@ -15,6 +19,9 @@ namespace BExIS.Modules.Lui.UI.Controllers
         // session variable names
         private static string SESSION_TABLE = "lui:resultTable";
         private static string SESSION_FILE = "lui:resultFile";
+
+        // namespace for download files
+        private static string FILE_NAMESPACE = Settings.get("lui:filename:namespace") as string;
         #endregion
 
         // GET: Main
@@ -28,6 +35,11 @@ namespace BExIS.Modules.Lui.UI.Controllers
             return View("Index", model);
         }
 
+        /// <summary>
+        /// trigger calculation of LUI values
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult CalculateLUI(LUIQueryModel model)
         {
@@ -46,5 +58,93 @@ namespace BExIS.Modules.Lui.UI.Controllers
 
             return PartialView("_results", results);
         }
+
+        /*
+        /// <summary>
+        /// prepare the serialized data file for download
+        /// </summary>
+        /// <param name="mimeType"></param>
+        /// <returns></returns>
+        public ActionResult PrepareDownloadFile(string mimeType)
+        {
+
+            // if we have already a matching file cached, we can short circuit here
+            if ((null != Session[SESSION_FILE]) && ((Dictionary<string, string>)Session[SESSION_FILE]).ContainsKey(mimeType))
+            {
+                return Json(new { error = false, mimeType = mimeType }, JsonRequestBehavior.AllowGet);
+            }
+
+            // helper class
+            OutputDataManager outputDataManager = new OutputDataManager();
+
+            // filename
+            // use unix timestamp to make filenames unique
+            string filename = Settings.get("lui:filename:download") as string;
+            // https://stackoverflow.com/a/17632585/1169798
+            Int32 unixTimestamp = (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            filename += "_" + unixTimestamp;
+
+            // datastructure ID
+            int dsId = (int)Settings.get("lui:datastructure");
+
+            // depends on the requested type
+            string path = "";
+            switch (mimeType)
+            {
+                case "text/csv":
+                case "text/tsv":
+                    path = outputDataManager.GenerateAsciiFile(FILE_NAMESPACE, Session[SESSION_FILE] as DataTable, filename, mimeType, dsId);
+                    break;
+
+                case "application/vnd.ms-excel.sheet.macroEnabled.12":
+                case "application/vnd.ms-excel":
+                    path = outputDataManager.GenerateExcelFile(FILE_NAMESPACE, Session[SESSION_FILE] as DataTable, filename, dsId);
+                    break;
+
+                default:
+                    Response.StatusCode = 420;
+                    return Json(new { error = true, msg = "Unknown file-type: " + mimeType }, JsonRequestBehavior.AllowGet);
+            }
+
+            // store path in session for further download
+            if (null == Session[SESSION_FILE])
+            {
+                Session[SESSION_FILE] = new Dictionary<string, string>();
+            }
+            ((Dictionary<string, string>)Session[SESSION_FILE])[mimeType] = path;
+
+            return Json(new { error = false, mimeType = mimeType }, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// return the serialized data file
+        /// if the file does not exist yet, it will be created
+        /// </summary>
+        /// <param name="mimeType"></param>
+        /// <returns></returns>
+        public ActionResult DownloadFile(string mimeType)
+        {
+
+            // make sure the file was created
+            if ((null == Session[SESSION_FILE]) || !((Dictionary<string, string>)Session[SESSION_FILE]).ContainsKey(mimeType))
+            {
+                ActionResult res = PrepareDownloadFile(mimeType);
+
+                // check, if everything went ok
+                if (200 != Response.StatusCode)
+                {
+                    return res;
+                }
+            }
+
+            // get file path
+            string path = ((Dictionary<string, string>)Session[SESSION_FILE])[mimeType];
+
+            // return file for download
+            return File(path, mimeType, Path.GetFileName(path));
+        }
+
+    */
+
     }
 }
