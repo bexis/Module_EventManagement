1195d375e30778ea86977e19e84489306a6bdae7
Eleonora Petzold <eleonora.petzold@yahoo.de>
2018-05-17 10:25:18 +0200

    include services to the module

---
+++ Services/BExIS.Emm.Services/Event/EventManager.cs
@@ -0,0 +1,101 @@
+﻿using BExIS.Dlm.Entities.MetadataStructure;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Vaiona.Persistence.Api;
+using E = BExIS.Emm.Entities.Event;
+
+namespace BExIS.Emm.Services.Event
+{
+    public class EventManager
+    {
+        public EventManager()
+        {
+            IUnitOfWork uow = this.GetUnitOfWork();
+            this.EventRepo = uow.GetReadOnlyRepository<E.Event>();
+        }
+
+        #region Data Readers
+
+        public IReadOnlyRepository<E.Event> EventRepo { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+
+        /// <summary>
+        /// Creates an EventRegistration <seealso cref="EventRegistration"/> and persists the entity in the database.
+        /// </summary>
+        public E.Event CreateEvent(string name, DateTime startDate, DateTime deadline, int participantsLimitation, bool editAllowed, string logInName, string logInPassword, MetadataStructure metadataStructure)
+        {
+            E.Event newEvent = new E.Event();
+            newEvent.Name = name;
+            newEvent.MetadataStructure = metadataStructure;
+            newEvent.StartDate = startDate;
+            newEvent.Deadline = deadline;
+            newEvent.ParticipantsLimitation = participantsLimitation;
+            newEvent.EditAllowed = editAllowed;
+            newEvent.LogInName = logInName;
+            newEvent.LogInPassword = logInPassword;
+
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.Event> repo = uow.GetRepository<E.Event>();
+                repo.Put(newEvent);
+                uow.Commit();
+            }
+
+            return newEvent;
+        }
+
+        /// <summary>
+        /// If the <paramref name="Activity"/> is not associated to any <see cref="Event"/>, the method deletes it from the database.
+        /// </summary>
+        public bool DeleteEvent(E.Event eEvent)
+        {
+            Contract.Requires(eEvent != null);
+            Contract.Requires(eEvent.Id >= 0);
+
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.Event> repo = uow.GetRepository<E.Event>();
+                eEvent = repo.Reload(eEvent);
+                repo.Delete(eEvent);
+                uow.Commit();
+            }
+
+            return true;
+        }
+
+        public E.Event UpdateEvent(E.Event eEvent)
+        {
+            Contract.Requires(eEvent != null);
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.Event> repo = uow.GetRepository<E.Event>();
+                repo.Put(eEvent);
+                uow.Commit();
+            }
+            return eEvent;
+
+        }
+
+        public IQueryable<E.Event> GetAllEvents()
+        {
+            return EventRepo.Query();
+        }
+
+        public E.Event GetEventById(long id)
+        {
+            return EventRepo.Query(u => u.Id == id).FirstOrDefault();
+        }
+
+
+        #endregion
+    }
+}
---
+++ Services/BExIS.Emm.Services/Event/EventRegistrationManager.cs
@@ -0,0 +1,103 @@
+﻿using E = BExIS.Emm.Entities.Event;
+using BExIS.Security.Entities.Subjects;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Vaiona.Persistence.Api;
+using System.Xml.Linq;
+
+namespace BExIS.Emm.Services.Event
+{
+    public class EventRegistrationManager
+    {
+        public EventRegistrationManager()
+        {
+            IUnitOfWork uow = this.GetUnitOfWork();
+            this.EventRegistrationRepo = uow.GetReadOnlyRepository<E.EventRegistration>();
+        }
+
+        #region Data Readers
+
+        public IReadOnlyRepository<E.EventRegistration> EventRegistrationRepo { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+
+        /// <summary>
+        /// Creates an EventRegistration <seealso cref="EventRegistration"/> and persists the entity in the database.
+        /// </summary>
+        public E.EventRegistration CreateEventRegistration(XmlDocument data, E.Event e, User user, bool deleted)
+        {
+            E.EventRegistration eventRegistration = new E.EventRegistration();
+            eventRegistration.Data = data;
+            eventRegistration.Deleted = deleted;
+            eventRegistration.Event = e;
+            eventRegistration.Person = user;
+
+
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.EventRegistration> repo = uow.GetRepository<E.EventRegistration>();
+                repo.Put(eventRegistration);
+                uow.Commit();
+            }
+
+            return eventRegistration;
+        }
+
+        /// <summary>
+        /// If the <paramref name="Activity"/> is not associated to any <see cref="Event"/>, the method deletes it from the database.
+        /// </summary>
+        public bool DeleteEventRegistration(E.EventRegistration eventRegistration)
+        {
+            Contract.Requires(eventRegistration != null);
+            Contract.Requires(eventRegistration.Id >= 0);
+
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.EventRegistration> repo = uow.GetRepository<E.EventRegistration>();
+                eventRegistration = repo.Reload(eventRegistration);
+                repo.Delete(eventRegistration);
+                uow.Commit();
+            }
+
+            return true;
+        }
+
+        public E.EventRegistration UpdateEventRegistration(E.EventRegistration eventRegistration)
+        {
+            Contract.Requires(eventRegistration != null);
+            using (IUnitOfWork uow = this.GetUnitOfWork())
+            {
+                IRepository<E.EventRegistration> repo = uow.GetRepository<E.EventRegistration>();
+                repo.Put(eventRegistration);
+                uow.Commit();
+            }
+            return eventRegistration;
+
+        }
+
+        public List<E.EventRegistration> GetAllRegistrationsByEvent(long id)
+        {
+            return EventRegistrationRepo.Query(a=>a.Event.Id == id).ToList();
+        }
+
+        public E.EventRegistration GetRegistrationByUserAndEvent(long userId, long eventId)
+        {
+            return EventRegistrationRepo.Query(a => a.Event.Id == eventId && a.Person.Id == userId).FirstOrDefault();
+        }
+
+        public int GetNumerOfRegistrationsByEvent(long id)
+        {
+            return EventRegistrationRepo.Query(a => a.Event.Id == id).Count();
+        }
+
+        #endregion
+    }
+}
