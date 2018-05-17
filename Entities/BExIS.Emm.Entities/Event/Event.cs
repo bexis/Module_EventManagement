baa12beae1e7c2640f5fd5a8fa0e219ea4aba9fc
Eleonora Petzold <eleonora.petzold@yahoo.de>
2018-05-17 10:24:02 +0200

    include entities to the module repo

---
+++ Entities/BExIS.Emm.Entities/Event/Event.cs
@@ -0,0 +1,46 @@
+﻿using BExIS.Dlm.Entities.MetadataStructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Vaiona.Entities.Common;
+
+namespace BExIS.Emm.Entities.Event
+{
+    public class Event : BusinessEntity
+    {
+        #region Attributes
+
+        public virtual string Name { get; set; }
+
+        public virtual DateTime StartDate { get; set; }
+
+        public virtual DateTime Deadline { get; set; }
+
+        public virtual int ParticipantsLimitation { get; set; }
+
+        public virtual bool EditAllowed { get; set; }
+
+        public virtual string LogInName { get; set; }
+
+        public virtual string LogInPassword { get; set; }
+
+
+        #endregion
+
+        #region Associations
+
+        public virtual MetadataStructure MetadataStructure { get; set; }
+
+        #endregion
+
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
---
+++ Entities/BExIS.Emm.Entities/Event/EventRegistration.cs
@@ -0,0 +1,39 @@
+﻿using BExIS.Security.Entities.Subjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Vaiona.Entities.Common;
+
+namespace BExIS.Emm.Entities.Event
+{
+    public class EventRegistration : BusinessEntity
+    {
+        #region Attributes
+
+        public virtual XmlDocument Data { get; set; }
+
+        public virtual bool Deleted { get; set; }
+
+
+        #endregion
+
+        #region Associations
+
+        public virtual User Person { get; set; }
+        public virtual Event Event { get; set; }
+
+
+        #endregion
+
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
+
