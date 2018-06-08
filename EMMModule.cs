8e32890af055796dc2823784b75b7356d4bc887b
Eleonora Petzold <eleonora.petzold@yahoo.de>
2018-06-08 12:50:29 +0200

    module files

--- EMMModule.cs
+++ EMMModule.cs
@@ -7,47 +7,32 @@ namespace BExIS.Modules.EMM.UI
 {
     public class EMMModule : ModuleBase
     {
-        public EMMModule(): base("EMM")
+        public EMMModule(): base("emm")
         {
+
         }
 
-        /// <summary>
-        /// Registers current area with the routing engine.
-        /// The default route is automatically registred. Using the AreaName as route prefix and url sapce.
-        /// </summary>
-        /// <remarks>
-        /// <list type="number">
-        ///     <item>If you are happy with the defaul route, either leave the method as is or comment it all (prefered).</item>
-        ///     <item>if you want to register other than the default, comment the call to the base method and write your own ones.</item>
-        ///     <item>If you want to register additional routes, write them after the call to the base method.</item>
-        /// </list>
-        /// </remarks>
-        /// <param name="context"></param>
-        //public override void RegisterArea(AreaRegistrationContext context)
-        //{
-        //    base.RegisterArea(context);
-        //    //context.MapRoute(
-        //    //    AreaName + "_default",
-        //    //    AreaName+"/{controller}/{action}/{id}",
-        //    //    new { action = "Index", id = UrlParameter.Optional }
-        //    //);
-        //}
+        public override void Start()
+        {
+            base.Start();
+            LoggerFactory.GetFileLogger().LogCustom("Start EMM");
+        }
 
-        //public override void Install()
-        //{
-        //    LoggerFactory.GetFileLogger().LogCustom("... start install of EMM ...");
-        //    try
-        //    {
-        //        base.Install();
-        //        EMMSeedDataGenerator.CreateFeatures();
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        LoggerFactory.GetFileLogger().LogCustom(e.Message);
-        //        LoggerFactory.GetFileLogger().LogCustom(e.StackTrace);
-        //    }
+        public override void Install()
+        {
+            LoggerFactory.GetFileLogger().LogCustom("... start install of EMM ...");
+            try
+            {
+                base.Install();
+                //EMMSeedDataGenerator.CreateFeatures();
+            }
+            catch (Exception e)
+            {
+                LoggerFactory.GetFileLogger().LogCustom(e.Message);
+                LoggerFactory.GetFileLogger().LogCustom(e.StackTrace);
+            }
 
-        //    LoggerFactory.GetFileLogger().LogCustom("... end install of EMM ...");
-        //}
+            LoggerFactory.GetFileLogger().LogCustom("... end install of EMM ...");
+        }
     }
 }
